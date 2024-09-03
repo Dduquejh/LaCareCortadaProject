0310a17c349b4692cedfadb1bb7d7549baf77e75
Dduquejh <johandanielduquehernandez@gmail.com>
2024-09-02 21:18:17 -0500

    feat: Create card slider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
-
+import { models } from './constants'
+import { CardSlider } from './components/CardSlider'
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-blue-700">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-700">
+        Hello world!
+      </h1>
+
+      <CardSlider list={models}/>
+
+    </>
   )
 }
 
