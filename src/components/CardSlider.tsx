8f5642db8462352d1247f450382d5415d9460d9a
Dduquejh <johandanielduquehernandez@gmail.com>
2024-09-08 09:45:46 -0500

    refactor: Responsive card slider

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -20,11 +20,17 @@ export const CardSlider = ( { list, showName = true }: CardSliderComponentProps)
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
-        centerMode: true
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {breakpoint: 1024, settings: {slidesToShow: 2, centerMode: false}},
+            {breakpoint: 768, settings: {slidesToShow: 1, centerMode: false}}
+        ]
     }
 
     return(
-        <div className="h-80 w-3/4 m-auto bg-black pt-6">
+        <div className="h-auto w-3/4 m-auto pt-6 pb-6">
             <Slider {...settings}>
                 {list.map((item, index) => (
                     <div key={index} className="px-5">
