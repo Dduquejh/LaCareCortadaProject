227fe51f83f0831a611983cd3ccf12ff600dc735
Dduquejh <johandanielduquehernandez@gmail.com>
2024-09-11 12:24:34 -0500

    Minimal changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 import "./App.css";
 import { RouterR } from "./components/Menu/RouterR";
-
-function App() {
-  return (
-    <>
-      <RouterR />
-=======
-import './App.css'
-import { RouterR } from './components/Menu/RouterR'
-import { useState } from 'react';
+import { useState } from "react";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
       <RouterR isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
->>>>>>> b4dad3e24ef329bbef3babaae1c120fe6b70e5b0
     </>
   );
 }
