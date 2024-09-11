e2543a1c5ff9954127c4e0e11a914630dc4eda83
throwmen <j.oliveros@ieciudaddonbosco.edu.co>
2024-09-11 17:43:29 -0500

    me dio pereza hacer un merge asi que lo voy a subir directamente a la main y que sea lo que dios quiera :p

--- src/components/Pages/AdminPanel.tsx
+++ src/components/Pages/AdminPanel.tsx
@@ -1,21 +1,25 @@
 import DropdownMenu from "../AdminPanel/DropdownMenu";
 
 const AdminPanel = () => {
-  return (
-    <div className="w-4/5 mx-auto my-10">
-      <h1 className="text-4xl font-bold text-white mb-4 text-center">
-        Admin Panel
-      </h1>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-16">
-          <DropdownMenu name="Products" type="product" />
-          <DropdownMenu name="Models" type="model" />
-          <DropdownMenu name="Events" type="event" />
-          <DropdownMenu name="Membership" type="membership" />
+    return (
+        <div className="flex flex-col min-h-screen">
+            <main className="flex-grow w-full">
+                <div className="w-full max-w-7xl mx-auto my-10 px-4">
+                    <h1 className="text-4xl font-bold text-white mb-4 text-center">
+                        Admin Panel
+                    </h1>
+                    <div className="flex justify-center">
+                        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16">
+                            <DropdownMenu name="Products" type="product" />
+                            <DropdownMenu name="Models" type="model" />
+                            <DropdownMenu name="Events" type="event" />
+                            <DropdownMenu name="Membership" type="membership" />
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AdminPanel;
